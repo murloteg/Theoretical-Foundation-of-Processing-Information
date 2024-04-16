bbffa6ee017fe304ccd683974d20bf525bde2d6c
core-assign <a.fomin1@g.nsu.ru>
2024-04-16 17:47:43 +0700

    done

--- application/main.py
+++ application/main.py
@@ -1,5 +1,6 @@
 import re
 import logging
+import extraction as ext
 
 info_logger = logging.getLogger("info-logger")
 info_logger.setLevel(logging.INFO)
@@ -12,7 +13,7 @@ def prepare_logger():
     info_logger.addHandler(handler)
 
 
-with open(file='resources/example.txt', mode='r') as file:
+with open(file='resources/example-3.txt', mode='r') as file:
     file_content = file.read()
 
 punctuation_marks = [".", ",", ":", ";", "?", "!", "\"", "'", "«", "»", "–", "-"]
@@ -26,8 +27,22 @@ def prepare_file_content_for_processing(*, file_content: str) -> str:
 
 def application_entrypoint():
     prepare_logger()
+
     prepared_file_content = prepare_file_content_for_processing(file_content=file_content)
     info_logger.info(f"Result of processing:\n\n{prepared_file_content}")
+
+    preprocessed_text = ext.preprocess_text(text=prepared_file_content)
+
+    raked_keywords = ext.extract_keywords_with_rake(text=prepared_file_content)
+    yaked_keywords = ext.extract_keywords_with_yake(text=preprocessed_text)
+    tfidf_keywords = ext.extract_keywords_with_tf_idf(text=preprocessed_text)
+
+    print("Raked algorithm result")
+    print(raked_keywords)
+    print("Yake! algorithm result")
+    print(yaked_keywords)
+    print("Tf-Idf algorithm result")
+    print(tfidf_keywords)
 
 
 if __name__ == "__main__":
